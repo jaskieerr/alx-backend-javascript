4e2e043d149b25c3be8581e3d38a01260f6452e6
jaskieerr <nizar.zairat@gmail.com>
2024-10-03 23:50:15 +0000

    fingers crossed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,44 +1,43 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = this._checkType(name, 'string', 'Name');
-    this._length = this._checkType(length, 'number', 'Length');
-    this._students = this._checkType(students, 'array', 'Students');
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    this._name = this._checkType(name, 'string', 'Name');
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(length) {
-    this._length = this._checkType(length, 'number', 'Length');
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(students) {
-    this._students = this._checkType(students, 'array', 'Students');
-  }
-
-  _checkType(value, type, variableName) {
-    const typeCheck = {
-      string: () => typeof value === 'string',
-      number: () => typeof value === 'number',
-      array: () => Array.isArray(value) && value.every((item) => typeof item === 'string'),
-    };
-
-    if (!typeCheck[type]()) {
-      throw new TypeError(`${variableName} must be a ${type}`);
+    constructor(name, length, students) {
+      if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+        throw new Error('Invalid input type');
+      }
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
-    return value;
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new Error('Invalid input type');
+      }
+      this._name = value;
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set length(value) {
+      if (typeof value !== 'number') {
+        throw new Error('Invalid input type');
+      }
+      this._length = value;
+    }
+  
+    get students() {
+      return this._students;
+    }
+  
+    set students(value) {
+      if (!Array.isArray(value)) {
+        throw new Error('Invalid input type');
+      }
+      this._students = value;
+    }
   }
-}
