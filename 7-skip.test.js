e49ed59dc9bb50b442b7033fc95a0866b56a6e58
jaskieerr <nizar.zairat@gmail.com>
2024-11-28 15:00:32 +0000

    sfsd

--- 7-skip.test.js
+++ 7-skip.test.js
@@ -10,7 +10,7 @@ describe('Testing numbers', () => {
   });
 
   it.skip('1 is equal to 3', () => {
-    expect(1 === 3).to.be.true; // This test is skipped
+    expect(1 === 3).to.be.true;
   });
 
   it('3 is equal to 3', () => {
